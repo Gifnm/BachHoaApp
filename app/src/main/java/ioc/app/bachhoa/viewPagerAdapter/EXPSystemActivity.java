916adcaecd133cb6af4a61da8e8e352acf34464a
Nguyen Minh Thu <98570646+Gifnm@users.noreply.github.com>
2023-11-25 13:25:14 +0700

    25/11/2023

---
+++ app/src/main/java/ioc/app/bachhoa/viewPagerAdapter/EXPSystemActivity.java
@@ -0,0 +1,16 @@
+package ioc.app.bachhoa.viewPagerAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ioc.app.bachhoa.R;
+
+public class EXPSystemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expsystem);
+    }
+}
